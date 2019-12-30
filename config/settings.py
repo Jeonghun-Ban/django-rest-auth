9716d980d58757ce755863ca118f27959c37b69b
Jeonghun-Ban <bjh5230@gmail.com>
2019-12-30 04:03:33 -0800

    modify settigs.py

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_auth'
+    'rest_auth',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
